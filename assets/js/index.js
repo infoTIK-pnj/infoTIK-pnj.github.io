437e72c2864e5fd866cb5b6ac6bace569c18cfee
lvciddreams <farrelmaahira104@gmail.com>
2024-11-27 01:33:21 +0700

    benerin blog page

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,16 +15,16 @@ const renderData = async () => {
     const contentContainer = document.getElementById('content-container');
     let cards = ``
     data.map((item) => {
-        cards += ` <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 lg:w-1/3 mt-3">
+        cards += ` <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:w-1/3 lg:w-1/5 my-4">
                     <a href="single-blog.html?blog=${item.slug}">
-                        <img class="rounded-t-lg object-cover" src="../assets/images/1.jpg" alt=""/>
+                        <img class="rounded-t-lg object-cover" src="${item.image}" alt=""/>
                     </a>
-                    <div class="p-5">
-                        <a href="#">
+                    <div class="p-6">
+                        <a href="single-blog.html?blog=${item.slug}">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${item.judul}</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${item}</p>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                        <a href="single-blog.html?blog=${item.slug}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
