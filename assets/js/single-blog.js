24b1b62adff7e8527251126f805da05548f602ed
lvciddreams <farrelmaahira104@gmail.com>
2024-11-26 17:52:03 +0700

    setup for working with the data

--- assets/js/single-blog.js
+++ assets/js/single-blog.js
@@ -1,43 +1,30 @@
-const data = [
-    {
-        judul : 'testing',
-        konten : 'testing',
-        image : './assets/images/1.jpg',
-        slug : 'testing'
-    },
-    {
-        judul : 'testing2',
-        konten : 'testing2',
-        image : './assets/images/1.jpg',
-        slug : 'testing2'
-    },
-    {
-        judul : 'testing3',
-        konten : 'testing3',
-        image : './assets/images/1.jpg',
-        slug : 'testing3'
-    },
-    {
-        judul : 'testing4',
-        konten : 'testing4',
-        image : './assets/images/1.jpg',
-        slug : 'testing4'
-    },
-]
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const value = urlParams.get('blog');
-
-const singleData = data.filter((item) => {
-    if(item.slug == value) {
-        return item
+const fetchData = async () => {
+    try {
+        const response = await fetch('./../assets/data.json');
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.log(error);
     }
-});
+}
+const renderData = async () => {
+    const data = await fetchData();
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const value = urlParams.get('blog');
 
-console.log(singleData);
+    const singleData = data.filter((item) => {
+        if (item.slug == value) {
+            return item
+        }
+    });
 
-const emptyContainer = document.getElementById('empty-container');
-let tester = `
-    <h1>${singleData[0].judul}</h1>
-`;
-emptyContainer.innerHTML = tester;
+    console.log(singleData);
+
+    const emptyContainer = document.getElementById('empty-container');
+    let tester = ` <h1>${singleData[0].judul}</h1> `;
+    let content = singleData[0].konten
+    emptyContainer.innerHTML = content;
+}
+
+renderData();
